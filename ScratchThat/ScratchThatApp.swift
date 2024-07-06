c6ee68026872ff675c0376a53471dd0ce7186071
Patryk <me@parpok.xyz>
2024-07-06 23:53:23 +0200

    Initial Commit

---
+++ ScratchThat/ScratchThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchThatApp.swift
+//  ScratchThat
+//
+//  Created by Patryk Puciłowski on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScratchThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
