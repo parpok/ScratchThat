efafc2574da2532d40cfa5eac65a0cbadb1a7767
Patryk <me@parpok.xyz>
2024-07-07 02:29:04 +0200

    Detecting now playing works. Now make it update

--- ScratchThat/ContentView.swift
+++ ScratchThat/ContentView.swift
@@ -5,15 +5,38 @@
 //  Created by Patryk Puciłowski on 06/07/2024.
 //
 
+import MediaPlayer
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isMediaOK = MPMediaLibrary.authorizationStatus()
+
+    @State private var stuff = MusicThings(songTitle: "", author: "")
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isMediaOK == .authorized {
+                Text("Now playing \(stuff.songTitle) by \(stuff.author)")
+                    .onAppear {
+                        stuff.recordPlaying()
+                    }
+            } else {
+                Text("Sorry you need to authorize use of Media player to work")
+
+                Button(action: {
+                    Task {
+                        let status = await MPMediaLibrary.requestAuthorization()
+                        DispatchQueue.main.async {
+                            self.isMediaOK = status
+                        }
+                    }
+                }) {
+                    Text("Ask for permission")
+                }
+                .buttonBorderShape(.roundedRectangle)
+                .buttonStyle(.borderedProminent)
+            }
+        }.onAppear {
+            isMediaOK = MPMediaLibrary.authorizationStatus()
         }
         .padding()
     }
@@ -21,4 +44,25 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+@Observable
+class MusicThings {
+    var songTitle: String
+    var author: String
+    func recordPlaying() {
+        let musicPlayer = MPMusicPlayerController.systemMusicPlayer
+        if let nowPlayingItem = musicPlayer.nowPlayingItem {
+            print(nowPlayingItem.title ?? "")
+            songTitle = nowPlayingItem.title ?? ""
+            author = nowPlayingItem.artist ?? ""
+        } else {
+            print("Nothing's playing")
+        }
+    }
+
+    init(songTitle: String, author: String) {
+        self.songTitle = songTitle
+        self.author = author
+    }
 }
